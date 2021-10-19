d282c6360a89aa29eb4a52155fba897bdf679543
Anatoliy Ostapenko <anatoliy.ostapenko@gmail.com>
2021-10-19 21:28:46 +0300

    add button

--- TwitterPredictor/ViewController.swift
+++ TwitterPredictor/ViewController.swift
@@ -6,14 +6,21 @@
 //
 
 import UIKit
+import SwifteriOS
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emotionLabel: UILabel!
+    
+    @IBOutlet weak var twitterTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    @IBAction func predictionButtonPressed(_ sender: UIButton) {
+    }
 }
 
